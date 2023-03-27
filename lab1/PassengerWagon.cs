f3361b5451f80a11d2bcee331a5ed7b25f85ace0
Timofey Krupkin <facemoon2606@gmail.com>
2023-03-27 20:23:18 +0300

    Небольшие изменения

--- lab1/PassengerWagon.cs
+++ lab1/PassengerWagon.cs
@@ -7,23 +7,19 @@ namespace lab1
 {
     public class PassengerWagon : IWagon
     {
-        
-
         public double Length { get; private set; }
         public bool IsPassenger { get; private set; }
         public double EmptyWeight { get; private set; }
         public double Weight { get; private set; }
         public int Payload { get; private set; }
-        public int count_of_passengers { get; private set; }
 
-
-        public PassengerWagon(double length, double emptyWeight, double payload)
+        public PassengerWagon(double length, double emptyWeight, int maxPassengers)
         {
             Length = length;
             EmptyWeight = emptyWeight;
             IsPassenger = false;
             Weight = emptyWeight;
-            Payload = payload;
+            Payload = maxPassengers;
         }
 
         public void Unload()
@@ -31,11 +27,10 @@ namespace lab1
             Weight = EmptyWeight;
         }
 
-        public void Load(Resource resource, int count)
+        public void Load(int passengersCount)
         {
-            count = count > Payload ? Payload : count < 0 ? 0 : count;
-            //loadPercentage = loadPercentage > 100 ? 100 : loadPercentage < 0 ? 0 : loadPercentage;
-            Weight += count*0.07d;
+            passengersCount = passengersCount > Payload ? Payload : passengersCount < 0 ? 0 : passengersCount;
+            Weight += passengersCount * 0.07d;
         }
     }
 }
